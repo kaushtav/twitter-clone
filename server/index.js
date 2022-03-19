3eb8b0c90cd3c47a72a80741fea3a0a073e18034
kaushtav <vickyatri.com@gmail.com>
2022-03-19 18:45:48 +0530

    Heroku test

--- server/index.js
+++ server/index.js
@@ -23,9 +23,9 @@ app.use(express.static(path.join(__dirname, 'client')));
 app.use('/api', getRoutes());
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/client/build')));
+  app.use(express.static(path.join(__dirname, '../client/build')));
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.join(__dirname,'..', 'client', 'build', 'index.html'));
   });
 } else {
   app.get('/', (req, res) => {
